b9cb49085966d8ccaa0c2e066f6997d694a19579
Puranprem <leonardo.figueroa.saldana@gmail.com>
2020-11-07 09:27:27 -0300

    actualizacion de documentos

--- 3_JAVASCRIPT/jasmin/jasmin_standalone/jasmine-standalone-3.6.0/spec/ejercicio2_Jasmin.js
+++
@@ -1,8 +0,0 @@
-//Ejercicio 2 - Es tu turno de escribir primero el test y luego la función
-//Ya sea en el mismo archivo o en uno diferente, crea una prueba o función que devuelva la suma del número más grande y el más pequeño del array. Por ejemplo, sumMaxMin([1,3,10]) debería devolver 10 + 1 = 11, y sumMaxMin([-2,-5,-10]) debería devolver -2 + -10 = -12. Una vez que hayas escrito la prueba, ejecútala (sin escribir toda la función para asegurarte de que fallen todas las pruebas), luego completa la función cerciorándote de que pase todas las pruebas. 
-
-//No utilices más de 30 minutos en todo el ejercicio.
-
-function mayMen(array) {
-    
-}

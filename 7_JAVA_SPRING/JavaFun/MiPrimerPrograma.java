43d0553f69f469475f7d301b2411e2c594784a2d
Puranprem <leonardo.figueroa.saldana@gmail.com>
2020-12-26 13:50:02 -0300

    Actividades Java fundamentos

---
+++ 7_JAVA_SPRING/JavaFun/MiPrimerPrograma.java
@@ -0,0 +1,7 @@
+public class MiPrimerPrograma{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Leonardo");
+        System.out.println("Tengo 47");
+        System.out.println("Mi ciudad es Puerto Montt, Chile");
+    }
+}
